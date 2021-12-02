00078821472f708c418c058968d4f7ee7e8162be
Malevick <mariondz91@yahoo.fr>
2021-12-02 14:55:01 +0100

    Design the cart page

--- assets/app.js
+++ assets/app.js
@@ -12,15 +12,9 @@ import './styles/app.css';
 import './bootstrap';
 
 import './styles/custom.scss';
-const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
-require('bootstrap');
+
+
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    console.log("yes")
-});
